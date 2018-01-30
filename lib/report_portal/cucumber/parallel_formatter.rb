97e1f5d7b6cb87c6055409d1c3c118328be961d1
Andrei Botalov <andrei_botalov@epam.com>
2018-01-30 15:01:52 +0300

    Unify common code in Cucumber Formatter and ParallelFormatter

--- lib/report_portal/cucumber/parallel_formatter.rb
+++ lib/report_portal/cucumber/parallel_formatter.rb
@@ -18,36 +18,20 @@
 
 require_relative 'formatter'
 require_relative 'parallel_report'
-require 'parallel_tests/gherkin/io'
 
 module ReportPortal
   module Cucumber
     class ParallelFormatter < Formatter
-      #include ::ParallelTests::Gherkin::Io
-
       # @api private
       def initialize(config)
-        ENV['REPORT_PORTAL_USED'] = 'true'
+        @start_launch_time = ReportPortal.now
+        super(config)
+      end
 
-        @queue = Queue.new
-        start_launch_time = ReportPortal.now
-        @thread = Thread.new do
-          @report = ReportPortal::Cucumber::ParallelReport.new(start_launch_time)
-          loop do
-            method_arr = @queue.pop
-            @report.public_send(*method_arr)
-          end
-        end
-        @thread.abort_on_exception = true
+      private
 
-        @io = config.out_stream
-
-        [:test_case_started, :test_case_finished, :test_step_started, :test_step_finished].each do |event_name|
-          config.on_event event_name do |event|
-            @queue.push([event_name, event, ReportPortal.now])
-          end
-        end
-        config.on_event :test_run_finished, &method(:on_test_run_finished)
+      def initialize_report
+        @report = ReportPortal::Cucumber::ParallelReport.new(@start_launch_time)
       end
     end
   end
--- lib/report_portal/cucumber/parallel_report.rb
+++ lib/report_portal/cucumber/parallel_report.rb
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'parallel_tests'
 require 'sys/proctable'
 
 require_relative 'report'
