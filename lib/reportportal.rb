9147ca84c3439088ca019946a74fa27ac6108a57
Omar Sawara <omar.sawara@samanage.com>
2020-01-08 11:57:37 +0200

    fix

--- lib/reportportal.rb
+++ lib/reportportal.rb
@@ -1,18 +1,18 @@
 # Copyright 2015 EPAM Systems
-# 
-# 
+#
+#
 # This file is part of Report Portal.
-# 
+#
 # Report Portal is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # ReportPortal is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public License
 # along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -141,7 +141,7 @@ module ReportPortal
           'tags': ['string']
         }
       do_request(url) do |resource|
-        resource.put data.to_json, content_type: :json, &@response_handler
+        resource.put force_finish_body.to_json, content_type: :json, &@response_handler
       end
     end
 
